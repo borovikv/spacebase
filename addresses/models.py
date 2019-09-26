e843fd64e646099ac19c6851aff5d6e7167af9b9
vladimir borovic <vladimir.borovic@ellation.com>
2019-09-26 23:01:13 +0300

    Add fuzzy find

--- addresses/models.py
+++ addresses/models.py
@@ -1,19 +1,41 @@
+import operator
+from functools import reduce
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Q
 
 
 class UserAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    street_address = models.CharField(max_length=255)
+    street_address = models.CharField(max_length=255, blank=True, null=True)
     street_address_line2 = models.CharField(max_length=255, blank=True, null=True)
     zipcode = models.CharField(max_length=12, blank=True, null=True)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=64, blank=True, null=True)
-    country = models.CharField(max_length=2)
+    country = models.CharField(max_length=2, blank=True, null=True)
     full_address = models.TextField(blank=True)
 
     def save(self, *args, **kwargs):
         streetdata = f"{self.street_address}\n{self.street_address_line2}"
         self.full_address = f"{streetdata}\n{self.zipcode} {self.city} {self.state} {self.country}"
         super().save(*args, **kwargs)
+
+    def __repr__(self):
+        return self.full_address
+
+    @staticmethod
+    def find(user, **kwargs):
+        queries = [UserAddress.build_query(key, value) for key, value in kwargs.items()]
+        result = UserAddress.objects.filter(*queries, user=user)
+        return result.first()
+
+    @staticmethod
+    def build_query(field_name, value):
+        if not value:
+            return Q()
+        result = Q(**{field_name: value})
+        if UserAddress._meta.get_field(field_name).null:
+            result |= Q(**{f'{field_name}__isnull': True})
+        return result | reduce(operator.or_, [Q(**{field_name: s}) for s in value.split()])
--- addresses/tests.py
+++ addresses/tests.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from addresses.models import UserAddress
+
+
+class TestUserAddressDeduplication(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user')
+
+    def test_find_by_name(self):
+        user2 = User.objects.create_user(username='user2')
+        UserAddress.objects.create(user=user2, name='Max', city='Giventown')
+        expected = UserAddress.objects.create(user=self.user, name='Max', city='Giventown')
+        result = UserAddress.find(user=self.user, name='Max Mustermann', city='Giventown')
+        self.assertEquals(result, expected)
+
+    def test_find_by_name_is_none(self):
+        result = UserAddress.find(user=self.user, name='Max Mustermann', city='Giventown')
+        self.assertIsNone(result)
+
+    def test_find_by_address(self):
+        UserAddress.objects.create(
+            user=self.user, name='Max Mustermann', street_address='Otherstreet', city='Giventown')
+        expected = UserAddress.objects.create(
+            user=self.user, name='Max Mustermann', street_address='Randomstreet', city='Giventown')
+        result = UserAddress.find(
+            user=self.user, name='Max Mustermann', street_address='456 Randomstreet', city="Giventown")
+        self.assertEquals(result, expected)
+
+    def test_find_by_address_when_address_is_null(self):
+        UserAddress.objects.create(user=self.user, name='Max', street_address='Otherstreet', city='Giventown')
+        expected = UserAddress.objects.create(user=self.user, name='Max', city='Giventown')
+        result = UserAddress.find(user=self.user, name='Max', street_address='Randomstreet', city="Giventown")
+        self.assertEquals(result, expected)
+
+    def test_find_by_address_when_different_address(self):
+        UserAddress.objects.create(user=self.user, name='Max', street_address='456 Randomstreet', city='Giventown')
+        result = UserAddress.find(user=self.user, name='Max', street_address='789 Otherstreet', city="Giventown")
+        self.assertIsNone(result)
+
+    def test_find_by_address_when_different_street_and_same_number(self):
+        UserAddress.objects.create(user=self.user, name='Max', street_address='789 Randomstreet', city='Giventown')
+        result = UserAddress.find(user=self.user, name='Max', street_address='789 Otherstreet', city="Giventown")
+        self.assertIsNone(result)
+
+    # def test_find_by_name(self):
+    #     UserAddress.objects.create(user=self.user, name='Max', city='Giventown')
+    #     UserAddress.objects.create(user=self.user, name='Max Mustermann', city='Giventown')
+    #     users = UserAddress.objects.filter(user=self.user).count()
+    #     self.assertEquals(users, 1)
